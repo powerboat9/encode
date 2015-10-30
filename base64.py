6ab333ddb924b00e2d99d32b210b4033388e864a
Milkey Mouse <milkeymouse@team-ivan.com>
2015-10-29 22:19:58 -0700

    made it work
    using the base64 lib
    
    i was bored

--- base64.py
+++ base64.py
@@ -1,7 +1,6 @@
+import base64
+
 bits = string.ascii_uppercase + string.ascii_lowercase + string.digits + "+/"
 
 def encode(txt):
-    stringArray = {}
-    for char in txt:
-        stringArray.append(ord(char))
-    for 
+    return base64.b64encode(txt, bits)
